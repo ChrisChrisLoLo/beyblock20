788550ad6d2334b5aa0c463ae9474fc153aca44a
ChrisChrisLoLo <sporewoh@gmail.com>
2022-09-11 20:26:10 -0500

    use custom matrix

--- firmware/beyblock20_controller/code.py
+++ firmware/beyblock20_controller/code.py
@@ -7,20 +7,6 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.scanners.keypad import MatrixScanner
 
-class MyKeyboard(KMKKeyboard):
-    def __init__(self):
-        # create and register the scanner
-        self.matrix = [MatrixScanner(
-            # required arguments:
-            column_pins=self.col_pins,
-            row_pins=self.row_pins,
-            # optional arguments with defaults:
-            columns_to_anodes=DiodeOrientation.COL2ROW,
-            interval=0.02,
-            max_events=64
-        )]
-        super().__init__()
-
 keyboard = KMKKeyboard()
 
 keyboard.keymap = [
--- firmware/beyblock20_controller/kb.py
+++ firmware/beyblock20_controller/kb.py
@@ -2,9 +2,21 @@ import board
 
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
+from kmk.scanners.keypad import MatrixScanner
 
 
 class KMKKeyboard(_KMKKeyboard):
+
     col_pins = (board.D6,board.D3,board.D2,board.D1,board.D0)
     row_pins = (board.D10,board.D9,board.D8,board.D7)
     diode_orientation = DiodeOrientation.COL2ROW
+
+    matrix = [MatrixScanner(
+        # required arguments:
+        column_pins=col_pins,
+        row_pins=row_pins,
+        # optional arguments with defaults:
+        columns_to_anodes=DiodeOrientation.COL2ROW,
+        interval=0.02,
+        max_events=64
+    )]
