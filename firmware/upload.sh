a70b351abf29f4b15d445216f95e5f8bbfbf8885
ChrisChrisLoLo <sporewoh@gmail.com>
2022-09-18 14:44:50 -0500

    update knoblin to v2.0

--- firmware/upload.sh
+++ firmware/upload.sh
@@ -28,7 +28,8 @@ deviceName=$1
 
 if [[ $deviceName == "bc" ]]; then
   #driveName="           "
-  driveName="NO NAME"
+  #driveName="NO NAME"
+  driveName="CIRCUITPY"
   # rsync -c ./beyblock20_controller/* /Volumes/CIRCUITPY/
   # # Copy over libraries
   # rsync -cr ./libs/kmk_firmware/kmk /Volumes/CIRCUITPY/
